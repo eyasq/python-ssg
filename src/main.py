d00f44f4216119baed2167aa1b5f1c7beaf335d4
eyasq <qeremeyas99@gmail.com>
2025-05-30 21:30:30 +0300

    final ver working

--- src/main.py
+++ src/main.py
@@ -3,13 +3,14 @@ from serve_webpage import generate_page, generate_pages_recursive
 import os
 import shutil
 import sys
+
 basepath = sys.argv[1] if len(sys.argv) > 1 else '/'
+
 def main():
     txtnode = TextNode('This is some anchor text', TextType.LINK, 'https://boot.dev')
     print(txtnode)
-    recursive_copy('/home/eyas/workspace/projects/ssg/python-ssg/static','/home/eyas/workspace/projects/ssg/python-ssg/public' )
+    recursive_copy('/home/eyas/workspace/projects/ssg/python-ssg/static','/home/eyas/workspace/projects/ssg/python-ssg/docs' )
     generate_pages_recursive('content/','template.html','docs/', basepath)
-
 
 def copy_folder_structure(src, dst):
     files_to_copy = os.listdir(src)
@@ -17,7 +18,6 @@ def copy_folder_structure(src, dst):
         if os.path.isfile(os.path.join(src, file)):
             print("Source path for file, and file name:", os.path.join(src, file), file)
             print("Destination path for file, and file name:", os.path.join(dst, file), file)
-
             shutil.copy(os.path.join(src,file), os.path.join(dst, file))
         else:
             os.mkdir(os.path.join(dst, file))
@@ -29,8 +29,5 @@ def recursive_copy(src, dst):
     os.mkdir(dst)
     copy_folder_structure(src, dst)
 
-
-main()
-
-
-
+if __name__ == "__main__":
+    main()
